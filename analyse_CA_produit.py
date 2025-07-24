b4a93d4dfae271f19c6ce1fead3fd022d3e9b5e5
Stanskis <165262083+Stanskis@users.noreply.github.com>
2025-07-24 16:06:03 +0000

    ajout try/except

--- analyse_CA_produit.py
+++ analyse_CA_produit.py
@@ -1,23 +1,27 @@
 import plotly.express as px
 import pandas as pd
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+try:
+    données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-# Chiffre d'affaires par produit
-données['CA'] = données['prix'] * données['qte']
+    # Chiffre d'affaires par produit
+    données['CA'] = données['prix'] * données['qte']
 
-df_ca_produit = données.groupby('produit').agg(
-    CA_par_produit=('CA','sum')
-).reset_index()
+    df_ca_produit = données.groupby('produit').agg(
+        CA_par_produit=('CA','sum')
+    ).reset_index()
 
-# Graphique
-figure = px.pie(
-    df_ca_produit,
-    values='CA_par_produit',
-    names='produit',
-    title="Chiffre d'affaires par produit (€)"
-)
+    # Graphique
+    figure = px.pie(
+        df_ca_produit,
+        values='CA_par_produit',
+        names='produit',
+        title="Chiffre d'affaires par produit (€)"
+    )
 
-figure.write_html('views/graph-CA-produit.html')
+    figure.write_html('views/graph-CA-produit.html')
 
-print('graph-CA-produit.html généré avec succès !')
+    print('graph-CA-produit.html généré avec succès !')
+
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
--- analyse_ventes_produit.py
+++ analyse_ventes_produit.py
@@ -1,21 +1,25 @@
 import plotly.express as px
 import pandas as pd
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+try:
+    données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-# Les ventes par produit
-df_ventes_produit = données.groupby('produit').agg(
-    Qte_par_produit=('qte','sum')
-).reset_index()
+    # Les ventes par produit
+    df_ventes_produit = données.groupby('produit').agg(
+        Qte_par_produit=('qte','sum')
+    ).reset_index()
 
-# Graphique
-figure = px.pie(
-    df_ventes_produit,
-    values='Qte_par_produit',
-    names='produit',
-    title="Les ventes par produit"
-)
+    # Graphique
+    figure = px.pie(
+        df_ventes_produit,
+        values='Qte_par_produit',
+        names='produit',
+        title="Les ventes par produit"
+    )
 
-figure.write_html('views/graph-ventes-produit.html')
+    figure.write_html('views/graph-ventes-produit.html')
 
-print('graph-ventes-produit.html généré avec succès !')
+    print('graph-ventes-produit.html généré avec succès !')
+
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
